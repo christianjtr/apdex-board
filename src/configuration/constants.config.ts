145b03d08c9eedf98ac283fe77c4c47e3afcc201
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2021-01-29 02:44:49 +0100

    Source files deleted

--- src/configuration/constants.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  MAX_TOP_APPS_BY_HOST: 25,
-  MAX_TOP_APPS_BY_HOST_CARD: 5,
-  TOGGLE_CTRL_LABEL: {
-    SHOW_AS_LIST: 'Show as list',
-    SHOW_AS_AWESOME_GRID: 'Show as an awesome grid'
-  }
-}
